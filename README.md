3e7b76fee0b7f40b1405ab6ff0e01cb5d157b8c3
rudeigerc <rudeigerc@gmail.com>
2018-04-02 18:58:42 +0800

    add bilibili support

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-# alfred-acg-detector [![Build Status](https://travis-ci.org/rudeigerc/alfred-acg-detector.svg?branch=master)](https://travis-ci.org/rudeigerc/alfred-acg-detector)
+# alfred-acg-detector
+[![Build Status](https://travis-ci.org/rudeigerc/alfred-acg-detector.svg?branch=master)](https://travis-ci.org/rudeigerc/alfred-acg-detector)
+[![version](https://img.shields.io/badge/version-0.1.0-blue.svg)](https://github.com/rudeigerc/alfred-acg-detector)
+[![license:mit](https://img.shields.io/badge/license-mit-blue.svg)](https://opensource.org/licenses/MIT)
 
-> acg-detector
+Inspired by [acg-detector](https://github.com/dyweb/acg-detector).
 
-
-## Install
+## Installation
 
 ```
-$ npm install --global alfred-acg-detector
+$ cd ~/Documents/Alfred.alfredpreferences/workflows/
+$ git clone https://github.com/rudeigerc/alfred-icolor.git
+$ cd alfred-icolor
+$ npm install
 ```
 
 *Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
 
-
-## Usage
-
-In Alfred, type `av`, <kbd>Enter</kbd>, and your query.
-
-
 ## License
 
-MIT © [rudeigerc](https://rudeigerc.github.io)
+MIT
--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 'use strict';
 const alfy = require('alfy');
+const htmlparser = require("htmlparser2");
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const id = alfy.input;
+const items = [];
+
+function fetchBilibili(id) {
+	const aid = id.substring(2);
+	const pagelistUrl = `https://api.bilibili.com/x/player/pagelist?aid=${aid}`;
+	const videoUrl = `https://www.bilibili.com/video/${id}`;
+
+	alfy.fetch(pagelistUrl).then(data => {
+		if (data.code === 0) {
+			if (data.data.length === 1) {
+				alfy.fetch(videoUrl, { json: false }).then(data => {
+					let handler = new htmlparser.DomHandler( (error, dom) => {
+						if (!error) {
+							const title = htmlparser.DomUtils.getElementsByTagName('title', dom)[0].children[0].data;
+							items.push({
+								title: title,
+								subtitle: id,
+								arg: videoUrl
+							});
+						} else {
+							items.push({
+								title: error,
+								subtitle: id
+							})
+						}
+					});
+					let parser = new htmlparser.Parser(handler);
+					parser.write(data);
+					parser.end();
+					alfy.output(items);
+				});
+			} else {
+				alfy.output(data.data.map(page => ({
+					title: page.part,
+					subtitle: id,
+					arg: `https://www.bilibili.com/video/${id}?p=${page.page}`
+				})));
+			}
+		} else {
+			alfy.output([{
+				title: data.code,
+				subtitle: id
+			}])
+		}
+	});
+}
+
+if (id.startsWith('av')) {
+	fetchBilibili(id);
+}
